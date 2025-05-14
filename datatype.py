753037056fee38b98271b3445d80baed898a7577
KKPANDEY261 <karanpandey261@gmail.com>
2025-05-15 00:33:27 +0530

    Datatype ineger,float, complex, string

---
+++ datatype.py
@@ -0,0 +1,25 @@
+# Numeric Types 
+                #Integer (int)
+a=10
+b=-5
+print("Intteger:-",a,b)
+
+# Float 
+c=3.14
+d=5e3
+print("float:-",c,d)
+
+# Complex 
+e=3+4j
+f=2j
+g=complex(1,-1)
+print(e,f,g)
+
+# Sequence Types
+     # String
+h='hello'
+i="hello"
+j='welcome to nsti howrah'
+k="welcome to nsti howrah"
+print(h,i,j,k)
+
