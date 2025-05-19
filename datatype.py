136dd36ef0a8ba8c667f8a722a4c3fe7380930e6
KKPANDEY261 <karanpandey261@gmail.com>
2025-05-19 17:01:54 +0530

    datatype

--- datatype.py
+++ datatype.py
@@ -19,7 +19,63 @@ print(e,f,g)
      # String
 h='hello'
 i="hello"
-j='welcome to nsti howrah'
-k="welcome to nsti howrah"
+j='''welcome to nsti howrah'''
+k="""welcome to nsti howrah"""
 print(h,i,j,k)
+# LIst
+x=[1,2,3,'hello']
+print(x)
+x[0]=10
+print(x)
 
+#Tuple
+l=(1,2,3,'hello')
+#l[0]=10
+print(l)
+
+#Range
+m=range(10)
+print(list(m))
+n=range(10,1,-2)
+print(list(n))
+
+#maping type
+karan={"name":'pandey','address':'howrah','mobile':7388847685}
+print(karan)
+karan['name']='karan'
+print(karan)
+
+#Set
+o={1,2,3,3,4,4,5,5,6}
+print(o)
+o.add(7)
+print(o)
+
+#Frozen set 
+p=frozenset([1,2,3,4,5,6,7])
+print(p)
+#p.add(8)
+#print(p)
+
+#boolean type
+q=True
+print(q)
+r=False
+print(r)
+
+#Binary Types
+#Bytes (bytes)
+s=b'hello'
+print(list(s))
+print(s)
+#s[0]=72
+#print(s)
+
+t=bytearray(b'hello')
+print(t)
+t[0]=72
+print(t)
+
+#None Type
+u=None
+print(u)
