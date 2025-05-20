aa9fcb51626ab019048473487766b8597e96a5f5
KKPANDEY261 <karanpandey261@gmail.com>
2025-05-20 12:17:28 +0530

    final submition

--- datatype.py
+++ datatype.py
@@ -3,18 +3,21 @@
 a=10
 b=-5
 print("Intteger:-",a,b)
+print(type(a))
 
 # Float 
 c=3.14
 d=5e3
 print("float:-",c,d)
-
+print(type(c))
 # Complex 
 e=3+4j
 f=2j
 g=complex(1,-1)
 print(e,f,g)
-
+print(type(e))
+print(type(f))
+print(type(g))
 # Sequence Types
      # String
 h='hello'
@@ -22,9 +25,14 @@ i="hello"
 j='''welcome to nsti howrah'''
 k="""welcome to nsti howrah"""
 print(h,i,j,k)
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
 # LIst
 x=[1,2,3,'hello']
 print(x)
+print(type(x))
 x[0]=10
 print(x)
 
@@ -32,36 +40,42 @@ print(x)
 l=(1,2,3,'hello')
 #l[0]=10
 print(l)
+print(type(l))
 
 #Range
 m=range(10)
 print(list(m))
 n=range(10,1,-2)
 print(list(n))
+print(type(n))
 
 #maping type
 karan={"name":'pandey','address':'howrah','mobile':7388847685}
 print(karan)
 karan['name']='karan'
 print(karan)
+print(type(karan))
 
 #Set
 o={1,2,3,3,4,4,5,5,6}
 print(o)
 o.add(7)
 print(o)
+print(type(o))
 
 #Frozen set 
 p=frozenset([1,2,3,4,5,6,7])
 print(p)
 #p.add(8)
 #print(p)
+print(type(p))
 
 #boolean type
 q=True
 print(q)
 r=False
 print(r)
+print(type(r))
 
 #Binary Types
 #Bytes (bytes)
@@ -70,12 +84,15 @@ print(list(s))
 print(s)
 #s[0]=72
 #print(s)
+print(type(s))
 
 t=bytearray(b'hello')
 print(t)
 t[0]=72
 print(t)
+print(type(t))
 
 #None Type
 u=None
 print(u)
+print(type(u))
--- operator.py
+++ operator.py
@@ -1,1 +1,95 @@
+#Arithmatic operator
+#addition
+a=5+5
+print(a)
+#Subtraction
+b=10-3
+print(b)
+#Multiplication
+c=5*5
+print(c)
+#Division
+d=5/2
+print(d)
+#Floor Division
+e=5//2
+print(e)
+#Modulus
+f=5%2
+print(f)
+#Exponentiation
+g=5**2
+print(g)
+
+#Comparision operator
+#Equal to
+a=5==5
+print("equal to",a)
+#Not equal to
+b=5!=6
+print(b)
+b=5!=5
+print(b)
+
+#Greater than
+
+print('Greater than',5>6)
+
+print('Greater than',5>4)
+
+#less than
+
+print('less than',5<3)
+
+print('less than',5<6)
+
+#Greater Than or equal
+
+print('Greater Than or equal',5>=5)
+print('Greater Than or equal',5>=4)
+print('Greater Than or equal',5>=3)
+
+
+#Lessthan or equal
+
+print('Lessthan or equal',5<=5)
+print('Lessthan or equal',5<=3)
+print('Lessthan or equal',5<=6)
+
+#Assignment operator
+
+#Assignment 
+
+a=10
+print('Assignment:-',a)
+a+=10
+print('Add and Assign:-',a)
+a-=5
+print('Subtract and assign:-',a)
+a*=5
+print('Multiply and assign:-',a)
+a/=4
+print('Divide and assign:-',a)
+a//=2
+print('Floor divide and assign:-',a)
+a%=2
+print('Modulus and assign:-',a)
+a**=3
+print('Exponent and assign:-',a)
+
+(y:=len("welcome"))
+print('Walrus operator:-',y)
+
+#Logical operator
+a=5>3
+b=3<4
+print('Logical and :-',a and b)
+c=5>3
+d=5<3
+print('Logical or :-',c or d)
+
+print('Logical not:-',not c)
+
+
+
 
